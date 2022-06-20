13fd79d47beef1ab51f9ec681352c42f8e3b5fb3
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-06-20 06:25:18 +0530

    feat : add create & update timestamps to users schema

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,35 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async getByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) return user;
+    throw new HttpException(
+      'User with this email does not exist',
+      HttpStatus.NOT_FOUND,
+    );
+  }
+  async create(userData: CreateUserDto) {
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...userData,
+        // createdAt: new Date(),
+        // updatedAt: new Date(),
+      },
+    });
+    return newUser;
+  }
+}
